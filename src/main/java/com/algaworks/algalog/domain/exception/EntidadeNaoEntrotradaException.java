04430dd64a173ce81b03f91c302a8fbdac3815a6
Karolaine Alencar <kalencar98@gmail.com>
2022-03-14 16:34:35 -0300

    Implementando busca de ocorrencias listar

---
+++ src/main/java/com/algaworks/algalog/domain/exception/EntidadeNaoEntrotradaException.java
@@ -0,0 +1,12 @@
+package com.algaworks.algalog.domain.exception;
+
+public class EntidadeNaoEntrotradaException extends NegocioException{
+
+	private static final long serialVersionUID = 1l;
+	
+	public EntidadeNaoEntrotradaException(String message) {
+		super(message);
+	}
+
+	
+}
